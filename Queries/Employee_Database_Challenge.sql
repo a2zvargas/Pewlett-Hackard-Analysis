8d2f71e1872ad472a6722eb46fd4aec23640e9d2
Amilcar Vargas <azvargas@gmail.com>
2021-08-12 19:32:40 -0400

    final upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -23,7 +23,15 @@ from unique_titles
 group by title
 order by count (title) desc;
 
+SELECT SUM(count) FROM retiring_titles;
+
 select * from retiring_titles;
+
+
+select count (*) from titles where title like '%Engineer%';
+
+select * from titles
+limit 20;
 
 select DISTINCT ON (e.emp_no) e.emp_no, 
 	e.first_name, 
@@ -40,9 +48,24 @@ WHERE de.to_date = ('9999-01-01')
 and birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 order by e.emp_no;
 
-select * from mentorship_eligibility;
+SELECT * FROM departments;
 
+SELECT COUNT(me.*), d.dept_name FROM mentorship_eligibility me
+LEFT JOIN dept_emp AS de ON de.emp_no = me.emp_no
+LEFT JOIN departments AS d ON d.dept_no = de.dept_no
+GROUP BY d.dept_name;
 
+select count(*) from mentorship_eligibility;
+
+SELECT ROUND((SELECT SUM(count) FROM retiring_titles) / (SELECT COUNT (*) FROM employees), 2) * 100;
+
+SELECT ROUND((SELECT SUM(count) FROM retiring_titles WHERE title like 'Senior%' ) / 
+			 (SELECT SUM(count) FROM retiring_titles), 2) * 100;
+
+SELECT ROUND((SELECT SUM(count) FROM retiring_titles WHERE title like '%Engineer%' ) / 
+			 (SELECT COUNT (*) FROM titles WHERE title LIKE '%Engineer%'), 2) * 100;
+
+SELECT ROUND((SELECT COUNT(*) FROM mentorship_eligibility) / (SELECT SUM(count) FROM retiring_titles), 2) * 100;
 
 
 
